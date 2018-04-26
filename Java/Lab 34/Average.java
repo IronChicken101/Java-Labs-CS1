e168091d35b050ba5d011ccd209e87ca1265f188
IronChicken101 <34466208+IronChicken101@users.noreply.github.com>
2018-04-26 11:54:47 -0500

    Add files via upload

---
+++ Java/Lab 34/Average.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Average
+{
+    public static void main(String[] args)
+    {
+        double one,two, average;
+        one = 5.0;
+        two = 5.0;
+        average = (one + two) / 2;
+        System.out.println(one + " + " + two + " has an average of " + average +"0");
+        System.out.println(" ");
+        double one1,two2, average3;
+        one1 = 90.0;
+        two2 = 100.0;
+        average3 = (one1 + two2) / 2;
+        System.out.println(one1 + " + " + two2 + " has an average of " + average3 +"0");
+        System.out.println(" ");
+        double one2,two3, average4;
+        one2 = 100.0;
+        two3 = 85.0;
+        average4 = (one2 + two3) / 2;
+        System.out.println(one2 + " + " + two3 + " has an average of " + average4 +"0");
+        System.out.println(" ");
+        double one3,two4, average5;
+        one3 = -100.0;
+        two4 = 55.0;
+        average5 = (one3 + two4) / 2;
+        System.out.println(one3 + " + " + two4 + " has an average of " + average5 +"0");
+    }
+}
---
+++ Java/Lab 34/SumUpStuff.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SumUpStuff
+{
+    public static void main (String[] args)
+    {
+    
+    }
+}
