259fe578c2d2f9ce1eee4bda74cc16f1149b1964
IronChicken101 <34466208+IronChicken101@users.noreply.github.com>
2018-04-26 12:00:57 -0500

    Add files via upload

---
+++ Java/Lab 41/Triangle.java
@@ -0,0 +1,56 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner;
+import java.lang.Math.*; 
+
+public class Triangle
+{
+	private int sideA, sideB, sideC;
+	private double perimeter;
+	private double theArea;
+
+	public Triangle()
+	{
+	   setSides(0,0,0);
+	   perimeter = 0;
+	   theArea = 0;
+	}
+
+	public Triangle(int a, int b, int c)
+	{
+	   setSides(a,b,c);
+	   perimeter = 0;
+	   theArea = 0;
+	}
+
+
+	public void setSides(int a, int b, int c)
+	{
+		sideA = a;
+		sideB = b;
+		sideC = c ;
+	}
+
+	public void calcPerimeter( )
+	{
+		perimeter = sideA + sideB + sideC;
+	}
+
+	public void calcArea( )
+	{
+		double s;
+		s = perimeter/2;
+		theArea = Math.sqrt(s *(s-sideA)*(s-sideB)*(s-sideC));
+	}
+
+	public void print( )
+	{
+		System.out.println("\n"+sideA  + " " + sideB + " " + sideC);
+		//System.out.print("Area == "+String.format("%.5f  \n", theArea)+"\n\n");
+		System.out.printf("Area ==  %.5f\n\n\n", theArea);
+	}
+}
